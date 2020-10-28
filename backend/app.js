d19d1213e05d70b94c3aff55ec347ad6c8b21aaf
samdedi24 <64523179+samdedi24@users.noreply.github.com>
2020-10-28 11:14:25 +0100

    Ajout bdd sequelize

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors')
+
+const app = express();
+app.use(cors())
+
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Votre requête a bien été reçue !' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Réponse envoyée avec succès !');
+});
+
+module.exports = app;
